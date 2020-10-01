372b835af6d6fe1ac32cc74f5b7b09d46e5116c0
Manuel De La Mora <Manuel@defaults-MBP.attlocal.net>
2020-10-01 16:32:22 -0700

    Built out base files with dependancies.

---
+++ app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Landing from './components/Landing.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "../scss/Landing.scss";
+
+function Landing(){
+    return(
+        <>
+            <h1>Hello</h1>
+            <canvas id="canvas"></canvas>
+        </>
+    )
+}
+
+export default Landing;
