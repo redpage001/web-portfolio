6ff5c2cd301c279c35dd873426a3234f967e43ef
Manuel De La Mora <Manuel@defaults-MBP.attlocal.net>
2020-10-08 18:31:38 -0700

    Edited bio work blerb and made tweaks to stylings to make the page more upbeat and pleasing.

--- app/src/components/Profile.js
+++ app/src/components/Profile.js
@@ -18,7 +18,7 @@ function Profile(){
                     />
                     
                     <div id="profileBioContainer">
-                        <p>I'm a web developer from Lambda School who's looking to create all kinds of content for everyone that's not only easy to use but are also fun and exciting! I used to be a graphic designer so I have a critical eye for layout and design for what is not only aesthetically appealing but easy to process. With those skills paired with what I learned at Lambda, I know I can create web applications that's exciting to look at and works fluidly with the user in mind.</p>
+                        <p>I'm a web developer from Lambda School who's looking to create all kinds of content for everyone that's not only easy to use but are also fun and exciting! I have experience in graphic design so I have a critical eye for layout and design for what is not only aesthetically appealing but easy to process. With those skills paired with what I learned at Lambda, I know I can create web applications that's exciting to look at and works fluidly with the user in mind.</p>
                     </div>
 
                 </div>
