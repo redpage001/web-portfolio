2df1daef76fd1b15f4c8eb2c6059f325b5c48943
Manuel De La Mora <Manuel@defaults-MBP.attlocal.net>
2020-10-14 19:16:09 -0700

    More work on the portfolio had been done to add images to the page for visual presentation.

--- app/src/components/Portfolio.js
+++ app/src/components/Portfolio.js
@@ -1,5 +1,11 @@
 import React from "react";
 
+import SaverLife from './../images/SaverLife.png'
+import SpotifyMusicSelector from './../images/SpotifyMusicSelector.png'
+import adventureMap from './../images/adventureMap.png';
+import GameOfLife from './../images/GameOfLife.png';
+import ReactBubbles from './../images/ReactBubbles.png';
+import YuGiOhCardGenerator from './../images/YuGiOhCardGenerator.png'
 import "../scss/Portfolio.scss";
 
 function Portfolio(){
@@ -10,40 +16,64 @@ function Portfolio(){
                 <h1>Portfolio</h1>
                 <div id="portfolioContent">
                     <div className="project">
-                        <div className="image">
-
+                        <div className="imageContainer">
+                            <img 
+                                className="image"
+                                src={SaverLife}
+                                alt="Image of SaverLife sample page"
+                            />
                         </div>
-                        <p>title</p>
+                        <p>SaverLife</p>
                     </div>
                     <div className="project">
-                        <div className="image">
-
+                        <div className="imageContainer">
+                            <img 
+                                className="image"
+                                src={SpotifyMusicSelector}
+                                alt="Image of Spotify Music Selector sample page"
+                            />
                         </div>
-                        <p>title</p>
+                        <p>Spotify Music Selector</p>
                     </div>
                     <div className="project">
-                        <div className="image">
-
+                        <div className="imageContainer">
+                            <img 
+                                className="image"
+                                src={adventureMap}
+                                alt="Image of Python: Adventure Game map visual"
+                            />
                         </div>
-                        <p>title</p>
+                        <p>Python: Adventure Game</p>
                     </div>
                     <div className="project">
-                        <div className="image">
-
+                        <div className="imageContainer">
+                            <img 
+                                className="image"
+                                src={GameOfLife}
+                                alt="Image of Conway's Game of Life sample page"
+                            />
                         </div>
-                        <p>title</p>
+                        <p>Conway's Game of Life</p>
                     </div>
                     <div className="project">
-                        <div className="image">
-
+                        <div className="imageContainer">
+                            <img 
+                                className="image"
+                                src={ReactBubbles}
+                                alt="Image of React Bubbles sample page"
+                            />
                         </div>
-                        <p>title</p>
+                        <p>React Bubbles</p>
                     </div>
                     <div className="project">
-                        <div className="image">
-
+                        <div className="imageContainer">
+                            <img 
+                                className="image"
+                                src={YuGiOhCardGenerator}
+                                alt="Image of YuGiOh Card Generator sample page"
+                            />
                         </div>
-                        <p>title</p>
+                        <p>YuGiOh Card Generator</p>
                     </div>
                 </div>
             </div>
